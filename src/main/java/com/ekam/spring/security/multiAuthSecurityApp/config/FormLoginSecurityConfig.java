aafe70404cf1ceee64695900028a779a846f7154
Aashay Saralkar <4965974+aashaysaralkar@users.noreply.github.com>
2018-11-02 15:00:14 +0530

    Allow requests for favicon.ico

--- src/main/java/com/ekam/spring/security/multiAuthSecurityApp/config/FormLoginSecurityConfig.java
+++ src/main/java/com/ekam/spring/security/multiAuthSecurityApp/config/FormLoginSecurityConfig.java
@@ -20,7 +20,7 @@ public class FormLoginSecurityConfig extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/webjars/**", "/images/**", "/security/**", "/error").permitAll().and()
+		http.authorizeRequests().antMatchers("/favicon.ico","/webjars/**", "/images/**", "/error").permitAll().and()
 				.antMatcher("/internal/**").authorizeRequests().and().formLogin().loginPage("/internal/login")
 				.permitAll().defaultSuccessUrl("/landing", true).and().logout().logoutUrl("/internal/logout")
 				.logoutSuccessUrl("/internal/login?logout").permitAll().and().csrf().disable();
